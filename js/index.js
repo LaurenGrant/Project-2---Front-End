4a56c22f41233a10e2734e7dc884f1c20081465a
Lauren Grant <lgrant1286@gmail.com>
2015-11-11 10:53:37 -0500

    Add working js to change tabs.
    Tabs are 'Log In' and 'Register' in user form.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 'use strict';
 
-onClick="document.getElementById('middle').scrollIntoView();"
+/*---- Changes Tabs in Login/Register Form ---- */
+
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+    $("#login-form").delay(100).fadeIn(100);
+    $("#register-form").fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+  $('#register-form-link').click(function(e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+});
