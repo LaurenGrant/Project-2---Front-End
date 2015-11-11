fc0d3c4b9bf7aedf404ad68e43b8e4b64b9faa8a
Lauren Grant <lgrant1286@gmail.com>
2015-11-11 13:54:20 -0500

    Adds create-event function.
    Tested in the terminal and it works.
    Saves in database.

--- js/index-ajax.js
+++ js/index-ajax.js
@@ -46,7 +46,45 @@ var api = {
       dataType: 'json'
     }, callback);
   },
+
+
+//Authenticated api actions
+  // listEvents: function list(token, callback) {
+  //   this.ajax({
+  //     method: 'GET',
+  //     url: this.url + '/events',
+  //     headers: {
+  //       Authorization: 'Token token=' + token
+  //     },
+  //     dataType: 'json'
+  //   }, callback);
+  // },
+
+  createEvent: function create(event, token, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/events/',
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify({event}),
+      dataType: 'json',
+    }, callback);
+  },
+
 };
+  // showEvent: function show(id, token, callback) {
+  //   this.ajax({
+  //     method: 'GET',
+  //     url: this.url + '/events/' + id,
+  //     headers: {
+  //       Authorization: 'Token token=' + token
+  //     },
+  //     dataType: 'json'
+  //   }, callback);
+  // },
+
 // $(document).ready(...
 $(function() {
   var form2object = function(form) {
@@ -106,7 +144,58 @@ $(function() {
     api.logout(id, token, callback);
   });
 
+// I think complete??
+// $('#list-events').on('submit', function(e) {
+//     var token = $(this).children('[name="token"]').val();
+//     e.preventDefault();
+//     api.listevents(token, callback);
+//   });
 
-});
+  var createEventCB = function createEventCB(err, data) {
+    if(err) {
+      callback(err);
+      return;
+    }
+
+    $('#eventId').val(data.event.id);
+    callback(null, data);
+  };
+
+  $('#create-event').on('submit', function(e) {
+    var event = {
+      // event: {
+      business_kind: $('#business_kind').val(),
+      name: $('#name').val(),
+      website: $('#website').val(),
+      phone_number: $('#phone_number').val(),
+      event_date: $('#event_date').val(),
+      group_size: $('#group_size').val(),
+      location_id: $('#location_id').val(),
+      user_id: $('#user_id').val()
+      }
+    // }
+    var token = $('.token').val();
+    var id = event.id;
+    e.preventDefault();
+    api.createEvent(event, token, createEventCB);
+
+    });
+
+    });
+  // $('#show-event').on('submit', function(e) {
+  //   var token = $(this).children('[name="token"]').val();
+  //   var id = $('#show-id').val();
+  //   e.preventDefault();
+  //   api.showEvent(id, token, callback);
+  // });
+// End of what I think works now.
+
+// });
+
+
+
+
+
+
 
 
